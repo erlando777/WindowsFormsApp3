c26cce7664d89439a6553963c1ed4cb3e1e8a22a
erlando777 <75303734+erlando777@users.noreply.github.com>
2020-12-03 22:48:59 +0200

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Eglutė";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(420, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(230, 207);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(296, 231);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(417, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Įveskite eglutės dydį:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Atsakymas:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(470, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "2. Užduotis";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(99, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 32);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Skaičiuoti";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Įveskite skaičių:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(96, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(173, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Patikrinimas ir pranešimas";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public static double[] Masyvas = new double[10];
+        int vieta = 0;
+        private object label;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            richTextBox1.Clear();
+            string skaicius = textBox1.Text;
+            if (String.IsNullOrWhiteSpace(skaicius) || String.IsNullOrEmpty(skaicius))
+            {
+                MessageBox.Show("Blogi duomenys.");
+            }
+            else if(Convert.ToInt32(skaicius) <= 0)
+            {
+                MessageBox.Show("Jūsų skaičius nėra teigiamas.");
+            }
+            else {
+                int kiekis = Convert.ToInt32(skaicius);
+                richTextBox1.Text = "Čia jūsų eglutė: \n";
+                int x = 1, tarp = 0;
+                for(int i = 0; i < kiekis; i++)
+                {
+                    if(x == 1 && x != kiekis)
+                    {
+                        int tarpai = kiekis - 1;
+                        while(tarpai > 0)
+                        {
+                            richTextBox1.Text += " ";
+                            tarpai--;
+                        }
+                        richTextBox1.Text += "* \n";
+                    }
+                    if(x != 1 && x != kiekis)
+                    {
+                        int tarpai1 = kiekis - x;
+                        while(tarpai1 > 0)
+                        {
+                            richTextBox1.Text += " ";
+                            tarpai1--;
+                        }
+                        tarp = x;
+                        while(tarp > 0)
+                        {
+                            richTextBox1.Text += "* ";
+                            tarp--;
+                        }
+                        richTextBox1.Text += "\n";
+                        x++;
+                    }
+                    if(x == kiekis)
+                    {
+                        while (x > 0)
+                        {
+                            richTextBox1.Text += "* ";
+                            x--;
+                        }
+                        break;
+                    }
+                    x++;
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            richTextBox1.Clear();
+            string tekstas = textBox2.Text;
+            if (String.IsNullOrEmpty(tekstas) || String.IsNullOrWhiteSpace(tekstas))
+            {
+                MessageBox.Show("Blogi duomenys");
+
+            }
+            else
+            {
+                textBox2.Clear();
+                
+                double skaicius = Convert.ToDouble(tekstas);
+                if (vieta < 10)
+                {
+                    Masyvas[vieta] = skaicius;
+                }
+                else
+                {
+                    MessageBox.Show("Masyve jau yra 10 elementų");
+                    Environment.Exit(0);
+                    
+                }
+                if (vieta == 9)
+                {
+                    label6.Text = "Jau užpildėte visus 10 elementų, atsakymas apačioje: ";
+                    double suma = 0;
+                    for (int i = 0; i < Masyvas.Length-1; i++)
+                    {
+                        suma += Masyvas[i];
+                    }
+                    richTextBox1.Text += "Suma lygi: \n" + suma + "\n\n";
+
+                }
+                else
+                {
+                    label6.Text = "Kad užduotis būtų įvykdyta, reikia įvesti dar: " + (Masyvas.Length - vieta - 1) + " elementa/us.";
+                    //MessageBox.Show("Masyve dar trūksta skaičių. Prašome įvesti papildomą skaičių.");
+                }
+
+                vieta++;
+                int n = Masyvas.Length;
+                richTextBox1.Text += "Jūsų masyvo nariai: \n";
+                for (int i = 0; i < vieta; i++)
+                {
+                    richTextBox1.Text += Masyvas[i] + " " ;
+                }
+                richTextBox1.Text += "\nElementų skaičius: " + vieta;
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
